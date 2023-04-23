a552f257fce9e35679816cc04b30d6a1b1e40844
Fares-Abdelkader <124618781+Fares-Abdelkader@users.noreply.github.com>
2023-04-23 16:36:09 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 /* global phaser */
 
+// Created by: Fares Abdelkader
+// Created on April 2023
+// This is the config file for the phaser3 program
+
+//* Game scene */
 const config = {
   type: Phaser.Auto,
   width: 1920,
   height: 1080,
-  backgroundcolor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set background color
+  backgroundcolor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autocenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
